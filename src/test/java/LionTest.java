8660a10ce159c4c8b9899b19982d592a9c4abefd
Eugene Em <mreugenem@gmail.com>
2024-09-24 17:28:06 +0500

    update after review: extraction of parameterized Lion tests into a separate class

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -3,22 +3,13 @@ import com.example.Lion;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
-public class LionTest {
 
-    private String sex;
-    private boolean expectedHasMane;
+public class LionTest {
 
     @Mock
     Feline feline;
@@ -28,10 +19,6 @@ public class LionTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    public LionTest(String sex, boolean expectedHasMane) {
-        this.sex = sex;
-        this.expectedHasMane = expectedHasMane;
-    }
 
     @Test
     public void testGetKittens() throws Exception {
@@ -39,21 +26,6 @@ public class LionTest {
         int kittens = lion.getKittens();
         //Ожидаем 0, тк мок Feline возвращает значение по умолчанию 0
         assertEquals(0, kittens);
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> getTestData() {
-        return Arrays.asList(new Object[][]{
-                {"Самец", true},
-                {"Самка", false}
-        });
-    }
-
-    @Test
-    public void testDoesHaveMane() throws Exception {
-        Lion lion = new Lion(sex, feline);
-        lion.doesHaveMane();
-        assertEquals(expectedHasMane, lion.doesHaveMane());
     }
 
     @Test
